9b4343a686e940259ac2c8a476804be237eece0b
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2022-11-15 12:52:30 -0300

    mn

---
+++ AprendizadoCSharp/01 - Condicionais/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Treinando Condicionais");
+            Console.WriteLine("======================");
+            Console.WriteLine("Entrada em Festa");
+            Console.WriteLine("======================");
+
+
+            Console.WriteLine("Bem Vindo, Qual seu Nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"{nome}, preciso que informe sua idade:");
+            int idade= int.Parse(Console.ReadLine());
+
+            if(idade >= 18)
+            {
+                Console.WriteLine($"{nome},você pode entrar na Festa");
+            }
+            else
+            {
+                Console.WriteLine($"{nome}, devido a sua idade sua entrada na festa não foi permitida, volte novamente tiver 18 anos ou mais.");
+            }
+
+           
+            Console.WriteLine("presione uma tecla para sair.");
+            Console.ReadLine();
+
+        }
+    }
+}
