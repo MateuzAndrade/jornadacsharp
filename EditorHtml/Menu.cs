4bb83e78d288989ff71ab33d812668e2b16e2a29
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2022-11-20 22:00:23 -0300

    aulas

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditorHtml
+{
+    public class Menu
+    {
+        public static void Show()
+        {
+
+        }
+    }
+}
