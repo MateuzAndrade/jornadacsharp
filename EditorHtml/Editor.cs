beed45a0c5a969e8e535d1cc925904bfaeebb2f7
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2022-11-23 06:35:01 -0300

    .

---
+++ EditorHtml/Editor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        // Modo de visualização
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor=con
+        }
+
+        public static void Start()
+        {
+
+        }
+    }
+}
--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -15,6 +15,9 @@ namespace EditorHtml
             DrawScreen(30,10);
             WriteOptions();
 
+
+            var Option = short.Parse(Console.ReadLine());
+            HandleMenuOption(Option);
         }
 
         public static void DrawScreen(int colum , int lines) {
@@ -43,17 +46,48 @@ namespace EditorHtml
             }
             Console.Write("+");
             Console.Write("\n");
-
-
         }
         
     
         public static void WriteOptions() {
             Console.SetCursorPosition(3,2);
-            Console.Write("Editor HTML");
-            Console.SetCursorPosition(3, 2);
-
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============="); 
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma Opção abaixo");
+            Console.SetCursorPosition(3,5);
+            Console.WriteLine("=============");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("3 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
         }
+    
+        public static void HandleMenuOption(short Option){
+            switch (Option)
+            {
+                case 1: 
+                    Console.WriteLine("Editor");
+                    break;
+                case 2:
+                    Console.WriteLine("View");
+                    break;
+                case 0:{
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show();
+                    break;
+            }
+        }
+    
+        
     }
     
 }
