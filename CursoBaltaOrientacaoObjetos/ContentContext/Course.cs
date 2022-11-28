77f716f010e61301fe8a6fd4fc259b70573d455b
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2022-11-28 19:26:06 -0300

    aulas

--- CursoBaltaOrientacaoObjetos/ContentContext/Course.cs
+++ CursoBaltaOrientacaoObjetos/ContentContext/Course.cs
@@ -9,21 +9,11 @@ namespace CursoBaltaOrientacaoObjetos.ContentContext
     {
         public Course()
         {
-            
+            Modules = new List<Module>();
         }
         public string? Tag { get; set; }
-        public IList<Module>? Modules { get; set; }       
+        public IList<Module> Modules { get; set; }       
 
     }
-    public class Module
-    {
-        public int Order { get; set; }
-        public string? Title { get; set; }       
-        
-    }
 
-    public class Lecture
-    {
-        
-    }
 }
---
+++ CursoBaltaOrientacaoObjetos/ContentContext/Lecture.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoBaltaOrientacaoObjetos.ContentContext
+{
+    public class Lecture
+    {
+        public int Ordem { get; set; }
+        public string? Title { get; set; }
+
+    }
+}
