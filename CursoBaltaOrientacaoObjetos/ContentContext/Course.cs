3c1346dd113279b3a6d0ea9c24e2236943005cb0
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2022-12-01 06:40:41 -0300

    aulas

--- CursoBaltaOrientacaoObjetos/ContentContext/Course.cs
+++ CursoBaltaOrientacaoObjetos/ContentContext/Course.cs
@@ -15,10 +15,7 @@ namespace CursoBaltaOrientacaoObjetos.ContentContext
         public string? Tag { get; set; }
         public IList<Module> Modules { get; set; }
         public int DurationInMinutes { get; set; }
-
         public EContentLevel Level { get; set; }
-        
-            
 
     }
 
